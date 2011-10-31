04b9c602404446a8dad9bdffb66a218e91b5791f
Joseph Hsu <jhsu.x1@gmail.com>
2011-10-31 16:26:50 -0400

    simplify api_key configuration

--- lib/ducksboard/configuration.rb
+++ lib/ducksboard/configuration.rb
@@ -1,21 +1,9 @@
 require 'yaml'
 module Ducksboard
   class Configuration
-    DEFAULT_RC_FILE = File.join(File.expand_path("~/"), ".ducksboardrc")
-
-    def self.file 
-      if File.exists? DEFAULT_RC_FILE
-        DEFAULT_RC_FILE
-      end
+    class << self
+      attr_accessor :api_key
     end
-
-    def self.api_key
-      if file
-        YAML.load_file(file)['api_key'].to_s
-      elsif api_key = ENV['DUCKSBOARD_API_KEY']
-        api_key
-      end
-    end
-
+    @api_key = ENV['DUCKSBOARD_API_KEY']
   end
 end
