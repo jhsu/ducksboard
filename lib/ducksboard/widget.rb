ca7ad6453d425240f7a3b9fef0e5993e2b4f3855
Joseph Hsu <jhsu.x1@gmail.com>
2011-11-01 11:51:52 -0400

    evaluate api_key on request

--- lib/ducksboard/widget.rb
+++ lib/ducksboard/widget.rb
@@ -3,7 +3,6 @@ module Ducksboard
   class Widget
     include ::HTTParty
     base_uri "https://push.ducksboard.com/values"
-    basic_auth Ducksboard.api_key, 'ducksboard-gem'
 
     attr_accessor :id, :data, :type
 
@@ -30,7 +29,10 @@ module Ducksboard
 
     def update(data=nil)
       @data = data if data
-      self.class.post('/' + id.to_s, :body => @data.to_json)
+      auth = {:username => ::Ducksboard.api_key, :password => "ducksboard-gem"}
+      self.class.post('/' + id.to_s,
+        :basic_auth => auth,
+        :body => @data.to_json)
     end
 
     def save
